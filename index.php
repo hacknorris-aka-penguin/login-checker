44a162105e88cf3b6bcf564adcd0cb3bbd56e3e5
Neo <hacknorris@tutanota.com>
2023-03-17 10:29:34 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <form method="post">
+      <input type="text" id="nick" placeholder="your nick:">
+      <input type="email" id="mail" placeholder="mail:">
+      <input type="password" id="password" placeholder="password:"> 
+      <input type="submit" id="enter" value="LOGIN"> 
+    </form>
+  </body>
+</html>
