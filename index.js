75bbc4411207f4b45556aedc4cf4056cb3610261
Neo <hacknorris@tutanota.com>
2023-03-17 11:31:38 +0000

    init add

---
+++ index.js
@@ -0,0 +1,28 @@
+const secureEmails = ["tutanota.com","proton.me"];
+const secureSocial = [];
+const numbers = [0,1,2,3,4,5,6,7,8,9];
+const letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+const uppercase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+function passwordCheck(password){
+
+}
+
+function socialCheck(socialList){
+
+}
+
+function getAccounts(nickname){
+
+}
+
+function securityMeter(socialList,passwordRate,mailRate){
+
+}
+
+function helper(socialList,passwordRate,mailRate){
+
+}
+
+function mailCheck(mail){
+
+}
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <html>
   <head>
+  <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <form method="post">
-      <input type="text" id="nick" placeholder="your nick:">
-      <input type="email" id="mail" placeholder="mail:">
-      <input type="password" id="password" placeholder="password:"> 
+    <form id="mainform" method="post">
+      <input type="text" id="nick" placeholder="your nick:" autocomplete="on" required><br>
+      <input type="email" id="mail" placeholder="mail:" autocomplete="on" required><br>
+      <input type="password" id="password" placeholder="password:" required><br>
       <input type="submit" id="enter" value="LOGIN"> 
     </form>
+  </body>
+</html>
+
+<html>
+  <head>
+  <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <div>pro tip: </div><br>
+    <div>security meter: </div><br>
+    <div>password meter: </div><br>
+    <div>social security meter:</div><br>
+    <div>email security: </div><br>
+    <div>full list of accounts: </div><br>
   </body>
 </html>
