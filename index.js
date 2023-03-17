6002f8c221bea9d028ce2313b45c7ad9fa25a741
Neo <hacknorris@tutanota.com>
2023-03-17 13:59:41 +0000

    update 2

--- index.js
+++ index.js
@@ -3,26 +3,43 @@ const secureSocial = [];
 const numbers = [0,1,2,3,4,5,6,7,8,9];
 const letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
 const uppercase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+
+/* from here code */
+
+var meter = {email:0,accounts:0,social:0,strength:0,general:0,tip:0};
+
 function passwordCheck(password){
 
+
+    meter.strength = outputStrength;
 }
 
 function socialCheck(socialList){
 
+
+    meter.social = outputSocial;
 }
 
 function getAccounts(nickname){
 
+
+    meter.accounts = outputAccounts;
 }
 
 function securityMeter(socialList,passwordRate,mailRate){
 
+
+    meter.general = outputGeneral;
 }
 
 function helper(socialList,passwordRate,mailRate){
 
+
+    meter.tip = outputTip;
 }
 
 function mailCheck(mail){
 
+    meter.email = outputMail;
 }
+
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <form id="mainform" method="post">
+    <form id="meterform" method="post">
       <input type="text" id="nick" placeholder="your nick:" autocomplete="on" required><br>
       <input type="email" id="mail" placeholder="mail:" autocomplete="on" required><br>
       <input type="password" id="password" placeholder="password:" required><br>
@@ -18,10 +18,22 @@
   </head>
   <body>
     <div>pro tip: </div><br>
+    <?php
+      echo "<script>document.writeln(meter.tip);</script>";
+    ?>
     <div>security meter: </div><br>
+    <meter onload="this.value=meter.general();">
     <div>password meter: </div><br>
+    <meter onload="this.value=meter.strength();">
     <div>social security meter:</div><br>
+    <meter onload="this.value=meter.social();">
     <div>email security: </div><br>
+    <?php 
+      echo "<script>document.writeln(meter.email);</script>";
+    ?>
     <div>full list of accounts: </div><br>
+    <?php 
+      echo "<script>document.writeln(meter.accounts);</script>";
+    ?>
   </body>
 </html>
