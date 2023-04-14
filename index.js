1e61039d16ef50ccb145558d0606ba12ed93f516
hacknorris <hacknorris@tutanota.com>
2023-04-14 23:12:30 +0200

    backup, some code

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const secureEmails = ["tutanota.com","proton.me","privatemail.com","anonaddy.me","countermail.com", "mailfence.com","librem.one","mailbox.org","runbox.com","startmail.com","posteo.net","posteo.eu","posteo.me","posteo.mx","posteo.org","posteo.co","r1.rpost.net","rpost.com","rpost.net","hushmail.com","servermx.com","sharklasers.com","guerrillamail.info","grr.la","guerrillamail.biz","spam4.me","pokemail.net","guerrillamailblock.com","guerrillamail.org","guerrillamail.com","guerrillamail.net","guerrillamail.de"];
 const secureSocial = [];
-const numbers = [0,1,2,3,4,5,6,7,8,9];
-const letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-const uppercase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+bad_pass_reg = /[0-9]/; 
+decent_pass_reg =  /[a-z|A-Z|a-zA-Z]/;
+good_pass_reg = /[a-zA-Z0-9]/; 
+very_good_pass_reg = /[.]/; 
+bad_pass_min = 0;
+decent_pass_min = 6;
+good_pass_min = 12;
+very_good_pass_min = 18;
 
 /* from here code */
 
 var meter = {email:0,accounts:0,social:0,strength:0,general:0,tip:0};
 
 function passwordCheck(password){
+switch (password){
+case bad_pass_reg :
 
+case decent_pass_reg:
+
+case good_pass_reg:
+
+case very_good_pass_reg:
+}
     meter.strength = outputStrength;
 }
 
